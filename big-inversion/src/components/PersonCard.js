beb6b73dda62562026a0a693ce6a8b4b9a6e9f4a
Silas Waller <97419274+silaswaller@users.noreply.github.com>
2022-04-28 14:46:34 -0500

    Add files via upload

--- big-inversion/src/components/PersonCard.js
+++ big-inversion/src/components/PersonCard.js
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
 class PersonCard extends React.Component {
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+            birthday: this.props.age,
+        };
+    }
+
     render() {
         return (
             <div>
                 <h1>{this.props.lastName}, {this.props.firstName}</h1>
                 <p>Age: {this.props.age}</p>
                 <p>Hair Color: {this.props.hairColor}</p>
+                <button onClick={() => this.setState({ birthday: this.state.birthday + 1 })}>Birthday Button for {this.props.firstName} {this.props.lastName}</button>
             </div>
         )
     }
